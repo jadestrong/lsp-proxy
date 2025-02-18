af3eb040e018cba94b6d6ae80f52c32f3f1c7be0
jadestrong <jadestrong@163.com>
2025-02-18 17:12:39 +0800

    fix: Reuse the buffer version of eglot

--- lsp-proxy.el
+++ lsp-proxy.el
@@ -191,9 +191,6 @@ The value can be:
 (defvar lsp-proxy--opened-buffers nil
   "List of buffers that have been opened in Lsp Proxy.")
 
-(defvar-local lsp-proxy--doc-version 0
-  "The document version of the current buffer. Incremented after each change.")
-
 (defvar-local lsp-proxy--recent-changes nil
   "Recent buffer changes as collected by `lsp-proxy--before-change'.")
 
@@ -774,7 +771,7 @@ Only works when mode is `tick or `alive."
 (defun lsp-proxy--on-doc-open ()
   "On doc open."
   (setq lsp-proxy--recent-changes nil
-        lsp-proxy--doc-version 0)
+        eglot--versioned-identifier 0)
   (when buffer-file-name
     (when (not (f-exists? buffer-file-name))
       (save-buffer))
@@ -785,7 +782,7 @@ Only works when mode is `tick or `alive."
                                                       :text (eglot--widening
                                                              (buffer-substring-no-properties (point-min) (point-max)))
                                                       :languageId ""
-                                                      :version lsp-proxy--doc-version))))))
+                                                      :version eglot--versioned-identifier))))))
 
 (defun lsp-proxy--on-doc-close (&rest _args)
   "Notify that the document has been closed."
@@ -812,7 +809,7 @@ Only works when mode is `tick or `alive."
     (let ((full-sync-p (eq :emacs-messup lsp-proxy--recent-changes)))
       (lsp-proxy--notify 'textDocument/didChange
                            (list :textDocument
-                                 (append (eglot--TextDocumentIdentifier) `(:version ,lsp-proxy--doc-version))
+                                 (eglot--VersionedTextDocumentIdentifier)
                                  :contentChanges
                                  (if full-sync-p
                                      (vector (list :text (eglot--widening
@@ -1920,7 +1917,7 @@ Returns a list as described in docstring of `imenu--index-alist'."
 (defun lsp-proxy--after-change (beg end pre-change-length)
   "Hook onto `after-change-functions'.
 Records BEG, END and PRE-CHANGE-LENGTH locally."
-  (cl-incf lsp-proxy--doc-version)
+  (cl-incf eglot--versioned-identifier)
   (pcase (and (listp lsp-proxy--recent-changes)
               (car lsp-proxy--recent-changes))
     (`(,lsp-beg ,lsp-end
